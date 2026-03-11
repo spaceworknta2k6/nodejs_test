eb5c5075ff4628658d34471b3d52815353a8e879
nta <spaceworknta2k6@gmail.com>
2026-03-11 21:35:11 +0700

    update views product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -5,6 +5,7 @@ module.exports.products = async (req, res) => {
     let find = {
         deleted : false
     }
+
     if(req.query.status === "active") {
         find.active = true
     }
@@ -12,10 +13,24 @@ module.exports.products = async (req, res) => {
         find.active = false
     }
     const product = await Product.find(find)
-    
+    const totalProducts = await Product.countDocuments({
+        deleted : false,
+    })
+    const activeProduct = await Product.countDocuments({
+        deleted : false,
+        active : true
+    })
+    const inactiveProduct = await Product.countDocuments({
+        deleted : false,
+        active : false
+    })
+
     res.render('admin/pages/product/index', {
         PageTitle : "Trang san pham",
         product : product,
-        status : req.query.status
+        status : req.query.status,
+        totalProducts : totalProducts,
+        activeProduct : activeProduct,
+        inactiveProduct : inactiveProduct
     })
 }
