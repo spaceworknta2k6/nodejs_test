aab58e9e8aad33625f5628f6efb33faedc468239
nta <spaceworknta2k6@gmail.com>
2026-03-12 00:31:16 +0700

    update submit button

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,36 +1,75 @@
 // [GET] /admin/product
-const Product = require('../../models/product.model')
+const Product = require("../../models/product.model");
 module.exports.products = async (req, res) => {
+  const status = req.query.status || "";
+  const keyword = req.query.keyword || "";
+  const sortQuery = req.query.sort || "";
 
-    let find = {
-        deleted : false
-    }
+  let find = {
+    deleted: false,
+  };
 
-    if(req.query.status === "active") {
-        find.active = true
-    }
-    if(req.query.status === "inactive") {
-        find.active = false
-    }
-    const product = await Product.find(find)
-    const totalProducts = await Product.countDocuments({
-        deleted : false,
-    })
-    const activeProduct = await Product.countDocuments({
-        deleted : false,
-        active : true
-    })
-    const inactiveProduct = await Product.countDocuments({
-        deleted : false,
-        active : false
-    })
+  if (req.query.status === "active") {
+    find.active = true;
+  }
+  if (req.query.status === "inactive") {
+    find.active = false;
+  }
+  // stat-card
+  const totalProducts = await Product.countDocuments({
+    deleted: false,
+  });
+  const activeProduct = await Product.countDocuments({
+    deleted: false,
+    active: true,
+  });
+  const inactiveProduct = await Product.countDocuments({
+    deleted: false,
+    active: false,
+  });
 
-    res.render('admin/pages/product/index', {
-        PageTitle : "Trang san pham",
-        product : product,
-        status : req.query.status,
-        totalProducts : totalProducts,
-        activeProduct : activeProduct,
-        inactiveProduct : inactiveProduct
-    })
-}
+  // end stat-card
+
+  // submitButton
+  if (keyword.trim()) {
+    const keywordRegex = new RegExp(keyword.trim(), "i");
+    find.$or = [
+      { title: keywordRegex },
+      { category: keywordRegex },
+      { description: keywordRegex },
+    ];
+  }
+
+  let sort = {};
+
+  switch (sortQuery) {
+    case "name-asc":
+      sort.title = 1;
+      break;
+    case "name-desc":
+      sort.title = -1;
+      break;
+    case "price-asc":
+      sort.price = 1;
+      break;
+    case "price-desc":
+      sort.price = -1;
+      break;
+    default:
+      sort._id = -1;
+      break;
+  }
+
+  const product = await Product.find(find).sort();
+  // end submitButton
+  res.render("admin/pages/product/index", {
+    PageTitle: "Trang san pham",
+    product: product,
+    status: status,
+    keyword: keyword,
+    sort: sortQuery,
+    totalProducts: totalProducts,
+    activeProduct: activeProduct,
+    inactiveProduct: inactiveProduct,
+  });
+};
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,19 +1,57 @@
-// dropdown
 const StatusFilter = document.querySelector("#statusFilter")
+const formInput = document.querySelector("#keyword")
+const sortFilter = document.querySelector("#sortFilter")
+const applyButton = document.querySelector("#apply")
+const resetButton = document.querySelector("#reset")
+// applyButton
+if(applyButton) {
+    applyButton.addEventListener("click", () => {
+        const keyword = formInput.value.trim()
+        const status = StatusFilter.value
+        const sort = sortFilter.value
 
-if(StatusFilter) {
-    StatusFilter.addEventListener("change", (e) => {
-        const status = e.target.value
+        const url = new URL(window.location.href)
 
-        let url = new URL(window.location.href)
+        if(keyword) {
+            url.searchParams.set("keyword", keyword)
+        } else {
+            url.searchParams.delete("keyword")
+        }
 
         if(status) {
             url.searchParams.set("status", status)
         } else {
             url.searchParams.delete("status")
         }
+
+        if(sort) {
+            url.searchParams.set("sort", sort)
+        } else {
+            url.searchParams.delete("sort")
+        }
+
         window.location.href = url.href
     })
 }
 
-// end dropdown
+// end applyButton
+
+// enterInput
+if(formInput) {
+    formInput.addEventListener("keydown", (e) => {
+        if(e.key === "Enter") {
+            applyButton.click();
+        }
+    })
+}
+// end enterInput
+
+
+// reset Button
+if(resetButton) {
+    resetButton.addEventListener("click", () => {
+        const url = window.location.origin + window.location.pathname
+        window.location.href = url
+    })
+}
+// end reset Button
