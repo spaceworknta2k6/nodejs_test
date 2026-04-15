28bd54e5357a50470b9923bcb2ea8de08b156b1c
nta <spaceworknta2k6@gmail.com>
2026-04-15 21:10:47 +0700

    config

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(cookieParser('keyboard cat'));
 app.use(session({ cookie: { maxAge: 60000 }}));
 app.use(flash());
 // end flash
-app.set('views', './views')
+app.set('views', `${__dirname}/views`)
 app.set('view engine', 'pug')
-app.use(express.static('public'))
+app.use(express.static(`${__dirname}/public`))
 
 const database = require('./config/database')
 database.connect();
